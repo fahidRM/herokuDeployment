421b1b793d4a6722a6f5f7fb36977f8320ef20ea
Fahid Mohammed <fahid.rabiu@gmail.com>
2021-10-26 20:06:18 +0100

    a

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var mongoose =  require('mongoose');
+var {Student} = require('models/student');
+
+
+
+mongoose.connect(
+  'mongodb+srv://app_user:app_user_123@cluster0.6kqdu.mongodb.net/authdb?retryWrites=true&w=majority'
+);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Fahid\'s App' });
 });
 
+
+router.get('/api/student', async function (req, res) {
+  const students = await Student.find();
+  return res.json(students);
+});
+
+
+router.post('/api/student', async function (req, res) {
+  const student = new Student({
+      username: req.body.username,
+      class: req.body.class,
+      rollnumber: req.body.rollnumber
+  });
+  await student.save();
+  res.json(student);
+});
 module.exports = router;
