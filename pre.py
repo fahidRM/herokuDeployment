a48062ad5239f1bcf6914f9708afa01a031429fb
Fahid RM <fahed@Lair.local>
2021-12-02 02:15:38 +0000

    init

---
+++ pre.py
@@ -0,0 +1,15 @@
+import dash
+from dash import dcc, html
+
+app = dash.Dash(__name__)
+
+# server = app.server
+
+app.layout = html.Div([
+    html.H2('Test deployment')
+])
+
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
